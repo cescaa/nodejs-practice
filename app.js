a0e9f5e678932f1e6f8e551c32aafbe83cb2ebf4
Cesca <32521038+cescaa@users.noreply.github.com>
2025-10-27 17:55:22 -0400

    Refactored

--- app.js
+++ app.js
@@ -4,36 +4,27 @@ const app = express();
 const PORT = 3000;
 app.use(express.json()); // middleware; provides .body on req
 
-/*
-// define route
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side.", app: "nodejs-practice" });
-});
-*/
-
 const incidentsData = [
   { incident_num: 1, desc: "Mock description..." },
   { incident_num: 2, desc: "Mock description..." },
   { incident_num: 3, desc: "Mock description..." },
 ];
 
-app.get("/api/v1/incidents", (req, res) => {
+const getAllIncidents = (req, res) => {
   res.status(200).json({
     // send json format standard
     status: "success",
     results: incidentsData.length,
     data: { incidents: incidentsData },
   });
-});
+};
 
-app.post("/api/v1/incidents", (req, res) => {
+const createNewIncident = (req, res) => {
   console.log(req.body);
   res.send("Done");
-});
+};
 
-app.get("/api/v1/incidents/:id", (req, res) => {
+const getIncident = (req, res) => {
   console.log(req.params.id);
 
   const id = req.params.id * 1; // convert to number type
@@ -47,9 +38,48 @@ app.get("/api/v1/incidents/:id", (req, res) => {
   );
   res.status(200).json({
     status: "success",
-    data: { selectedIncident },
+    data: { incident: selectedIncident },
   });
-});
+};
+
+const updateIncident = (req, res) => {
+  if (req.params.id * 1 > incidentsData.length)
+    return res
+      .status(404)
+      .json({ status: "fail", message: "Invalid ID for PATCH" });
+
+  res.status(200).json({
+    status: "success",
+    data: { incident: "Upated data here..." },
+  });
+};
+
+const deleteIncident = (req, res) => {
+  if (req.params.id * 1 > incidentsData.length)
+    return res
+      .status(404)
+      .json({ status: "fail", message: "Invalid ID for DELETE" });
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
+
+/*
+app.get("/api/v1/incidents", getAllIncidents);
+app.post("/api/v1/incidents", createNewIncident);
+app.get("/api/v1/incidents/:id", getIncident);
+app.patch("/api/v1/incidents/:id", updateIncident);
+app.delete("/api/v1/incidents/:id", deleteIncident);
+*/
+
+app.route("/api/v1/incidents").get(getAllIncidents).post(createNewIncident);
+app
+  .route("/api/v1/incidents/:id")
+  .get(getIncident)
+  .patch(updateIncident)
+  .delete(deleteIncident);
 
 // run server
 app.listen(PORT, () => {
