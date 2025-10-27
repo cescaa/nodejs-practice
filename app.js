81dc0753bf371bc560e5d66d76c81a3a48c8c63b
Cesca <32521038+cescaa@users.noreply.github.com>
2025-10-27 16:13:31 -0400

    GET and POST routes added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express"); // import express
 
 const app = express();
 const PORT = 3000;
+app.use(express.json()); // middleware; provides .body on req
 
 /*
 // define route
@@ -12,16 +13,42 @@ app.get("/", (req, res) => {
 });
 */
 
-const incidentsData = [{ incident_num: 1, desc: "Mock description..." }];
+const incidentsData = [
+  { incident_num: 1, desc: "Mock description..." },
+  { incident_num: 2, desc: "Mock description..." },
+  { incident_num: 3, desc: "Mock description..." },
+];
 
 app.get("/api/v1/incidents", (req, res) => {
-  res
-    .status(200)
-    .json({
-      status: "success",
-      results: incidentsData.length,
-      data: { incidents: incidentsData },
-    });
+  res.status(200).json({
+    // send json format standard
+    status: "success",
+    results: incidentsData.length,
+    data: { incidents: incidentsData },
+  });
+});
+
+app.post("/api/v1/incidents", (req, res) => {
+  console.log(req.body);
+  res.send("Done");
+});
+
+app.get("/api/v1/incidents/:id", (req, res) => {
+  console.log(req.params.id);
+
+  const id = req.params.id * 1; // convert to number type
+  if (id > incidentsData.length)
+    return res
+      .status(404)
+      .json({ status: "fail", message: "Invaid Incident Number" });
+
+  const selectedIncident = incidentsData.find(
+    (elem) => elem.incident_num === id
+  );
+  res.status(200).json({
+    status: "success",
+    data: { selectedIncident },
+  });
 });
 
 // run server
